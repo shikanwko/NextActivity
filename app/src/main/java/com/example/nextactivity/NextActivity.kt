cd4995799e3e1b94ac05eecb863d65404d18661e
afarisfudin <afarisfudin@gmail.com>
2022-08-24 16:38:52 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nextactivity/NextActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nextactivity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_next)
+    }
+}
